9ecee026d70cf08646485bf0d8d94b64b2154f5b
Pavel Pachobut <p.pachobut@gothammgt.ca>
2026-04-16 11:05:39 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ImageMagick;
+
+const string imagePath = "sample.jpg";
+
+if (!File.Exists(imagePath))
+{
+	Console.Error.WriteLine($"Could not find {imagePath} in the current working directory.");
+	return;
+}
+
+using (var image = new MagickImage(imagePath))
+{
+	image.Quality = 100;
+	image.Format = MagickFormat.Png;
+
+	MemoryStream stream = new MemoryStream();
+	image.Write(stream);
+}
