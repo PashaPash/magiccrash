d37a05f26f8623883c901ce630c16c49590edc9e
Pavel Pachobut <p.pachobut@gothammgt.ca>
2026-04-17 16:45:21 -0400

    upd

--- Program.cs
+++ Program.cs
@@ -8,11 +8,20 @@ if (!File.Exists(imagePath))
 	return;
 }
 
-using (var image = new MagickImage(imagePath))
+try
 {
-	image.Quality = 100;
-	image.Format = MagickFormat.Png;
+	using var imageStream = File.OpenRead(imagePath);
+	using (var image = new MagickImage(imageStream))
+	{
+		image.Quality = 100;
+		image.Format = MagickFormat.Png;
 
-	MemoryStream stream = new MemoryStream();
-	image.Write(stream);
+		MemoryStream stream = new MemoryStream();
+		image.Write(stream);
+	}
+}
+catch (Exception ex)
+{
+	// not executed, segfault instead
+	Console.WriteLine($"Caught: {ex.GetType().Name}: {ex.Message}");
 }
